8f95fb741df4ed30651e41b9dd1bc67af1437b52
zahan.dhrubo <zahan.dhrubo@grameenphone.com>
2023-12-21 09:55:52 +0600

    balance response added

--- BotResponse.py
+++ BotResponse.py
@@ -22,7 +22,7 @@ addfund = {
         "Well, something is better than nothing, right {name}? Here's your new balance: {balance} Tk."
     ],
     "<500":[
-        "You understand it's not dollars, right? Adding a mere {amount} Tk. isn't going to help you that much {name}. Anways, here's your balance: {balance} Tk.",
+        "You understand it's not dollars, right 😒? Adding a mere {amount} Tk. isn't going to help you that much {name}. Anways, here's your balance: {balance} Tk.",
         "Well, the tong isn't that far. So I guess it's okay having {balance} Tk. in your balance {name}",
         "When life gives you lemon 🍋 you gotta make lemonade 🍹. You have to do something like that with your {balance} Tk. balance {name}"
     ]
@@ -42,7 +42,7 @@ pay = {
     "<0":[
         "Damm {name}! You're like reverse Bruce Wayne.\n\nNo, you're not Batman, you're poor. Here's your balance: {balance} Tk.",
         "{item} made you broke {name}. Here's your balance: {balance} Tk.",
-        "Oh you poor little {name}! You're now in debt. This is your balance after you had {item}: {balance} Tk.",
+        "Oh you poor little {name} 🥺! You're now in debt. This is your balance after you had {item}: {balance} Tk.",
         "I know {name}, {item} is worth going broke for. Here's your balance though: {balance} Tk.",
         "Umm, do you need help cleaning those dishes {name}? Your balance is {balance} Tk."
     ],
@@ -55,5 +55,60 @@ pay = {
         "Nothing better than a{n} {item} in the {daytype}. Here's your balance {name}: {balance} Tk.",
         "Good {daytype} {name} 😊! You had a{n} {item}. Here's your balance: {balance} Tk.",
         "No one deserves {item} more than you {name}. You've earned it! You still have {balance} Tk. in your balance"
+    ]
+}
+
+balance = {
+    "3k":[
+        "All hail the Sovereign Ruler {name} 👑! There is {balance} Tk. in your treasury, your highness!",
+        "Let's go on a tour ✈️ {name}! You have {balance} Tk. now in your fund. We could go to Hawaii 🏖️",
+        "Do you know what comes to my mind when I see you {name}?\n\nThis: 💸💸💸\n\nHere's your balance Richie Rich: {balance} Tk.",
+        "Are you planning on buying the whole store or what {name}? Here's your balance: {} Tk.",
+        "DAMN 🤯! Your balance is my monthly salary 😭! {balance} Tk.?! What will you do with all these money {name}?"
+    ],
+    "2k":[
+        "You can buy lots of coffee ☕ with this, {name}. Here's your balance: {balance} Tk",
+        "Buy whatever you want {name}. You have {balance} Tk. in your balance",
+        "Good day {name} 😊! Here's your balance: {balance} Tk."
+    ],
+    "1k":[
+        "Buy whatever you want {name}. You have {balance} Tk. in your balance",
+        "Good day {name} 😊! Here's your balance: {balance} Tk."
+    ],
+    "500":[
+        "At least you're not completely broke {name}. You only have {balance} Tk. now in your account",
+        "Well, something is better than nothing, right {name}? Here's your new balance: {balance} Tk."
+    ],
+    "100":[
+        "You understand it's not dollars, right 😒? Anways, here's your balance: {balance} Tk.",
+        "Well, the tong isn't that far. So I guess it's okay having {balance} Tk. in your balance {name}",
+        "When life gives you lemon 🍋 you gotta make lemonade 🍹. You have to do something like that with your {balance} Tk. balance {name}",
+        "Oh I wish I could give you some money, {name}. Your balance is {balance} Tk.",
+        "You should add fund, {name}. You only have {balance} Tk. in your balance",
+        "Ugh! I hate it when broke people ask me to check balance 😤. Here's your balance 😒 {name}: {balance} Tk."
+    ],
+    ">0":[
+        "You understand it's not dollars, right 😒? Anways, here's your balance: {balance} Tk.",
+        "Well, the tong isn't that far. So I guess it's okay having {balance} Tk. in your balance {name}",
+        "When life gives you lemon 🍋 you gotta make lemonade 🍹. You have to do something like that with your {balance} Tk. balance {name}",
+        "Oh I wish I could give you some money, {name}. Your balance is {balance} Tk.",
+        "You should add fund, {name}. You only have {balance} Tk. in your balance",
+        "Ugh! I hate it when broke people ask me to check balance 😤. Here's your balance 😒 {name}: {balance} Tk.",
+        "You can't even afford a coffee {name} 😒. Here's your balance: {balance} Tk."
+    ],
+    "0":[
+        "Who invented the number 0 🤔?\n\n{name}'s bank balance 🤣",
+        "You're almost a billionair {name} 🤯!\n\nYou're only a billion dollar short 🤣",
+        "You ran out of money {name} 😨!. You're balance is 0 Tk.",
+        "Good day Aryabhatta! Your balance is your favourite number: 0",
+        "Oh you poor little {name} 🥺. Your balance is 0 Tk.",
+        "If you double your balance, it stays the same. Guess what your balance is {name}",
+        "Your balance isn't hard to remember {name}. It's 0 Tk. 🤣"
+    ],
+    "<0":[
+        "Damm {name}! You're like reverse Bruce Wayne.\n\nNo, you're not Batman, you're poor. Here's your balance: {balance} Tk.",
+        "Oh you poor little {name} 🥺! You're now in debt. This is your balance: {balance} Tk.",
+        "Umm, do you need help cleaning those dishes {name}? Your balance is {balance} Tk.",
+        "Ugh! I hate it when broke people ask me to check balance 😤. Here's your balance 😒 {name}: {balance} Tk."
     ]
 }
