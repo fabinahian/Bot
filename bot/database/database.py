326d4e328f18f988ebb13710da70281bd952e660
zahan.dhrubo <zahan.dhrubo@grameenphone.com>
2024-08-07 12:30:06 +0600

    removed unnecessary directory creation

--- bot/database/database.py
+++ bot/database/database.py
@@ -3,9 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from bot.config import Config
 
-# Ensure the 'data' directory exists
-if not os.path.exists('data'):
-    os.makedirs('data')
 
 engine = create_engine(Config.DATABASE_URL)
 Session = sessionmaker(bind=engine)
