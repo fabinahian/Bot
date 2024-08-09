82f500392cd911ad390d468bfe82c193b02cfa0c
zahan.dhrubo <zahan.dhrubo@grameenphone.com>
2024-08-09 15:45:41 +0600

    private and group mention separated

--- bot/handlers/chat/chat.py
+++ bot/handlers/chat/chat.py
@@ -1,5 +1,5 @@
 from telegram.ext import ContextTypes
-from telegram import Update
+from telegram import Update, constants
 from bot.logging_config import logger, logging
 from bot.response.response import generate_response
 from bot.handlers.common import get_user_info
@@ -15,7 +15,12 @@ async def mention(update: Update, context: ContextTypes.DEFAULT_TYPE):
     settings = GPT_Settings()
     settings.system["content"] = """You are TabaqBillBot. You are sarcastic and funny and like to make dark jokes.
                                     If someone sends you a message, you reply accordilngly. Keep it interesting"""
-    if "@TabaqBillBot" in chat_text:
+
+    if update.message.chat.type == constants.ChatType.GROUP and f"@{context.bot.username}" in chat_text:
         prompt = chat_text
         response = generate_response(prompt, settings=settings)
         await update.message.reply_text(response)
+    elif update.message.chat.type == constants.ChatType.PRIVATE:
+        prompt = chat_text
+        response = generate_response(prompt, settings=settings)
+        await context.bot.send_message(chat_id = update.effective_chat.id, text = response)
