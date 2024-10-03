d0fec81aa27f60f499bc9576f8d5ff110f6f93f2
Fabi Nahian Madhurja <fabinahian1@gmail.com>
2024-10-04 00:07:19 +0600

    Initial Railway setup

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 from bot.main import start_bot
 from bot.logging_config import logger, logging
+from bot.database.database import init_db
 
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    logger.info("Starting bot")
+    logger.info("Initializing the database...")
+    # Initialize the database and create tables if they don't exist
+    init_db()
+    
+    logger.info("Database initialized successfully.")
+    logger.info("Starting bot...")
+    
+    # Start the bot after the database is initialized
     start_bot()
